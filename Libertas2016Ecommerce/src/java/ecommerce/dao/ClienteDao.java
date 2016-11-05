cc85f9896e9fedcc64750baf7c0e8782fa7bc817
Bruno <Bruno@note-bruno>
2016-11-05 00:22:26 -0200

    Dao

---
+++ Libertas2016Ecommerce/src/java/ecommerce/dao/ClienteDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecommerce.dao;
+
+import ecommerce.entidade.Cliente;
+
+/**
+ *
+ * @author Bruno
+ */
+public class ClienteDao extends Dao<Cliente>{
+
+    public ClienteDao() {
+        super(Cliente.class);
+    }
+    
+}
