9691afa54bb7b5473c2126063bee52bde7efcac8
Bruno <Bruno@note-bruno>
2016-11-07 21:35:16 -0200

    Comite das classes dao

---
+++ Libertas2016Ecommerce/src/java/ecommerce/dao/CidadeDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecommerce.dao;
+
+import ecommerce.entidade.Cidade;
+
+/**
+ *
+ * @author Bruno
+ */
+public class CidadeDao extends Dao<Cidade>{
+
+    public CidadeDao() {
+        super(Cidade.class);
+    }
+    
+}
