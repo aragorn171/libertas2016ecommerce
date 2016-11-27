e6b3acb24e65577d21921cc74801620d9c3b384b
Plínio Naves <pliniopjn@gmail.com>
2016-11-27 18:02:19 -0200

    Jersey setup

--- Libertas2016Ecommerce/src/java/ecommerce/webservice/resources/ClienteResource.java
+++ Libertas2016Ecommerce/src/java/ecommerce/webservice/resources/ClienteResource.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ecommerce.webservice.resources;
 
 import ecommerce.dao.ClienteDao;
@@ -24,18 +23,43 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/cliente")
 public class ClienteResource {
-    
+
     private final ClienteDao dao;
-    
+
     public ClienteResource() {
         this.dao = new ClienteDao();
     }
-    
+
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Cliente> findAll() {
-            return dao.listar();
+        return dao.listar();
     }
-    
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cliente find(@PathParam("id") Integer id) {
+        return dao.consultar(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String post(Cliente cliente) {
+        //return (dao.inserir(cliente)) ? "Cliente '" + cliente.getNome() + "' inserido!" : "Erro ao inserir cliente'" + cliente.getNome() + "'!";
+        dao.inserir(cliente);
+        return "Cliente inserido!";
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String put(Cliente cliente) {
+        //return (dao.update(cliente)) ? "Operacao '" + cliente.getNome() + "' alterada!" : "Erro ao alterar operacao'" + cliente.getNome() + "'!";
+        dao.alterar(cliente);
+        return "Cliente alterado!";
+    }
+
 }
